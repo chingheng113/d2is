337ad5ed55c9afd98e05fdcbcec244714009a224
Ching-Heng Lin <chingheng113@gmail.com>
2021-05-11 10:56:18 +0800

    bk

---
+++ hpc/do_recon-all.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+#SBATCH --partition=container
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=64
+#SBATCH --job-name=Jim-recon-all
+#SBATCH --output=/home/chingheng/recon-all.out
+#SBATCH --error=/home/chingheng/recon-all.err
+module purge
+module load singularity/v3.6.2
+cd /home/chingheng
+echo "Running on hosts: $SLURM_NODELIST"
+echo "Running on $SLURM_NNODES nodes."
+echo "Running $SLURM_NTASKS tasks."
+singularity exec d2is_v0.1.sif /home/chingheng/reconall.sh
