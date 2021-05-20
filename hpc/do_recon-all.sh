14d8748fd7729ecd0e4a37902b561e83beda2106
Ching-Heng Lin <chingheng113@gmail.com>
2021-05-20 15:38:41 +0800

    bk

--- hpc/do_recon-all.sh
+++ hpc/do_recon-all.sh
@@ -3,12 +3,12 @@
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=64
 #SBATCH --job-name=Jim-recon-all
-#SBATCH --output=/home/chingheng/recon-all.out
-#SBATCH --error=/home/chingheng/recon-all.err
+#SBATCH --output=/home/chingheng/play_ground/recon-all.out
+#SBATCH --error=/home/chingheng/play_ground/recon-all.err
 module purge
 module load singularity/v3.6.2
-cd /home/chingheng
+cd /home/chingheng/play_ground/
 echo "Running on hosts: $SLURM_NODELIST"
 echo "Running on $SLURM_NNODES nodes."
 echo "Running $SLURM_NTASKS tasks."
-singularity exec d2is_v0.1.sif /home/chingheng/reconall.sh
+singularity exec d2is.sif /home/chingheng/play_ground/reconall.sh
--- hpc/reconall.sh
+++ hpc/reconall.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
-cd /home/chingheng/T1WI_org
-time ls *.nii.gz | parallel --jobs 1 recon-all -parallel -openmp 64 -i {} -s {.}_result -all -qcache
+cd /home/chingheng/play_ground/data_1
+export FREESURFER_HOME=/opt/freesurfer
+export SUBJECTS_DIR=/home/chingheng/play_ground/data_1
+ls *.nii.gz | parallel --jobs 1 recon-all -parallel -openmp 64 -i {} -s {.}_result -all -qcache
